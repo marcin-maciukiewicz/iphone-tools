8d611478de3ede682c5fb23dd79cc029d68d21d5
ciukes <ciukes@gmail.com>
2009-11-06 23:21:24 +0000

    import

---
+++ scripts/dump-sdk-symbols.sh
@@ -0,0 +1,6 @@
+
+SDK_ROOT="/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS"
+SDK_VERSION="3.1"
+find ${SDK_ROOT}${SDK_VERSION}.sdk/System/Library/Frameworks -type f -perm 0755 | xargs nm 
+
+#>> ~/tmp/iPhoneOS${SDK_VERSION}.sdk.symbols
